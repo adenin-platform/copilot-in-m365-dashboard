cbd8f427fb6314407f85cbcaa48619286544d674
Ollie <olliejm@protonmail.com>
2024-08-01 10:30:58 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-dotnet publish ./adenin.PnPTool -p:PublishProfile=FolderProfile
-powershell Compress-Archive .\adenin.PnPTool\bin\Release\net8.0\publish\win-x64 .\build.zip -Update
+dotnet publish ./adenin.PnpTool -p:PublishProfile=FolderProfile
+powershell Compress-Archive .\adenin.PnpTool\bin\Release\net8.0\publish\win-x64 .\build.zip -Update
